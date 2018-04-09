452a0730b39eef864b95ba33da1da1eb098794ea
Shadman Chowdhury <shadman.c19@bronxsoftware.org>
2018-04-09 12:40:25 +0000

    index.html

---
+++ js/glossary.js
@@ -0,0 +1,6 @@
+/*global $*/
+/*global ready*/
+$(document).ready(function() {
+	$('#fullpage').fullpage();
+	$('.scrolla').scrolla();
+});
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,7 @@
 /*global $*/
 /*global ready*/
 $(document).ready(function(){
-    $("#title").typeIt({
-        speed: 100,
-        lifeLike: true,
-    });
-    $('#sec1').paroller();
-    $('#sec3').paroller();
     $(".scrolla").scrolla({
         mobile:true
     });
-    $(".dropdown-trigger").dropdown();
 })
